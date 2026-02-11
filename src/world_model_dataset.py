8ebc554d3bc01ea2116e3c1a4c7d6ebd7c3a151b
ssrhaso <hasaana2005@gmail.com>
2026-02-11 21:54:46 +0000

    FEAT: Loading games init

--- src/world_model_dataset.py
+++ src/world_model_dataset.py
@@ -11,11 +11,22 @@ from typing import List, Tuple
 import yaml
 
 class WorldModelDataset(Dataset):
-    def __init__(self):
+    def __init__(
+        self,
+        games : List[str],
+        tokens_dir : str = "checkpoints/rsvq_tokens",
+        replay_dir : str = "data",
+        seq_len : int = 64,
+    ):
         super().__init__()
+        self.seq_len = seq_len
+        
+        self.all_tokens = []        # (N, 3) arrays, 1x per game
+        self.all_actions = []       # (N,) arrays, 1x per game
+        self.valid_starts = []      # (game_idx, start_idx) tuples 
     
     def __len__(self):
-        pass
+        return len(self.valid_starts)
     
     def __getitem__(self, idx):
         pass
